ad3290ca25163d0636450242136e85daa88f2c15
Pavel Kapersky <Dragsterrffillerr@gmail.com>
2023-10-24 11:36:26 +0400

    Articles

--- views/site/single.php
+++ views/site/single.php
@@ -12,8 +12,9 @@ use yii\helpers\Url;
                     </div>
                     <div class="post-content">
                         <header class="entry-header text-center text-uppercase">
-                            <h6><a href="<?=Url::toRoute(['site/category','id'=>$article->category->id])?>"> <?= $article->category->title?></a></h6>
-
+                            <?php if ($article->category !== null): ?>
+                                <h6><a href="<?= Url::toRoute(['/site/category', 'id' => $article->category->id]) ?>"> <?= $article->category->title?></a></h6>
+                            <?php endif; ?>
                             <h1 class="entry-title"><a href="<?=Url::toRoute(['site/view','id'=>$article->id])?>"><?= $article->title?></a></h1>
 
 
